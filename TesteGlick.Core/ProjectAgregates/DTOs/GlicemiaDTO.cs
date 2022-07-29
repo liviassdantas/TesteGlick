5137759cd6d4f50dde36b0a429d4a3102fd04b0c
Livia Dantas <livia.ss.dantas@gmail.com>
2022-07-28 21:12:58 -0300

    DTOs iniciais

---
+++ TesteGlick.Core/ProjectAgregates/DTOs/GlicemiaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGlick.Core.ProjectAgregates.DTOs
+{
+    public class GlicemiaDTO
+    {
+        public Int32 Id { get; set; }
+        public Int32 IdPaciente { get; set; }
+        public Int16 Valor { get; set; }
+        public DateTime DataHora { get; set; }
+
+    }
+}
---
+++ TesteGlick.Core/ProjectAgregates/DTOs/PacienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGlick.Core.ProjectAgregates.DTOs
+{
+    public class PacienteDTO
+    {
+        public Int32 Id { get; set; }
+        public String Nome { get; set; }
+        public Int32 Idade { get; set; }
+        public Boolean Diabetico { get; set; }
+    }
+}
